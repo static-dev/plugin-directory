afbb001ae6e228e86010e7c560d6ddb4a21c6007
Jeff Escalante <jescalan@users.noreply.github.com>
2016-08-23 18:42:01 -0400

    basic search functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,36 @@
-console.log('you can use ES6 here : )')
+/* global plugins */
+const $ = document.querySelector.bind(document)
+const $a = document.querySelectorAll.bind(document)
+const list = plugins.map((p, i) => { p.id = `.p${i}`; return p })
+
+$('.search').onsubmit = (e) => {
+  e.preventDefault()
+
+  // get the search value
+  const val = $('.field').value
+  const re = new RegExp(val)
+
+  // search relevant module fields for matches
+  const matches = list.reduce((m, p) => {
+    if (p.module.name.match(re)) { m.push(p.id); return m }
+    if (p.module.description.match(re)) { m.push(p.id); return m }
+    if (p.module.publisher.username.match(re)) { m.push(p.id); return m }
+    if (p.module.keywords && p.module.keywords.indexOf(val) > -1) {
+      m.push(p.id); return m
+    }
+    return m
+  }, [])
+
+  // clear any existing search classes
+  $a('tr:not(.header)').forEach((el) => {
+    el.className = el.className.replace(' hidden', '')
+  })
+
+  // add classes to hide non-matching elements
+  if (matches.length) {
+    const toHide = $a(`tr:not(.header):not(${matches.join('):not(')})`)
+    toHide.forEach((el) => {
+      el.className = `${el.className} hidden`
+    })
+  }
+}
--- views/index.sml
+++ views/index.sml
@@ -12,14 +12,14 @@ extends(src='layout.sml')
       input(type='submit')
 
     table
-      tr
+      tr.header
         th Name
         th Description
         th Quality
         th Author
         th Links
-      each(loop='p of plugins')
-        tr
+      each(loop='p, i of plugins')
+        tr(class='p{{ i }}')
           td: strong {{ p.module.name }}
           td {{ p.module.description }}
           td {{ Math.round(((p.score.detail.quality + p.score.detail.maintenance) / 2) * 100) }}
@@ -28,3 +28,5 @@ extends(src='layout.sml')
             .links
               a.github(href='{{ p.module.links.repository }}') gh
               a.npm(href='{{ p.module.links.npm }}') npm
+
+    script var plugins = {{{ JSON.stringify(plugins) }}}
